8bf2e9bc7e432491d2114f564d06518456dcb45f
Sve-AR <sverreabdouryeng@gmail.com>
2026-01-06 12:15:20 +0100

    ni commit 2026

--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -3,17 +3,46 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+let textPart1Line1 = "";
+let textPart1Line2 = "";
+for (let i = 1, j = 10; i <= 10; i++, j--) {
+  textPart1Line1 += " " + i;
+  textPart1Line2 += " " + j;
+}
+printOut(textPart1Line1);
+printOut(textPart1Line2);
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const Numberguesser = 40;
+let part1Random = Math.floor(Math.random() * 60) + 1;
+while (part1Random !== Numberguesser) {
+  part1Random = Math.floor(Math.random() * 60) + 1;
+}
+printOut("Yes! you guessed correct: " + part1Random);
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const Numberguesser2 = 5743;
+let guesses = 0;
+let part3Random = 0;
+
+const TimeGuess = Date.now(); //starter å "record" her
+
+while (part3Random !== Numberguesser2) {
+    part3Random = Math.floor(Math.random() * 1000000) + 1;
+    guesses++;
+}
+let endGuess = Date.now(); //slutter "record" her
+let taken = endGuess - TimeGuess;
+
+printOut("Guesses " + guesses);
+printOut("Tid " + taken + "ms");
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
